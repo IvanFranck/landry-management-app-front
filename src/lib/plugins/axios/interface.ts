458da70d994f54fc467867fd452e543ba8ccf862
ivan nzima <nzimaivan007@gmail.com>
2024-02-17 12:48:13 +0100

    handling log in process

---
+++ src/lib/plugins/axios/interface.ts
@@ -0,0 +1,7 @@
+export type GenericAxiosResponse = {
+    message: string
+    status: number,
+    error: string,
+    path: string,
+    timestamp: string
+}
--- src/views/login-view.tsx
+++ src/views/login-view.tsx
@@ -4,26 +4,43 @@ import { z } from "zod"
 import { FormControl, FormField, FormItem, FormLabel, Form } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-
-const formSchema = z.object({
-    password: z.string(),
-    phone: z.number().min(9, 'Numéro invalide').max(9)
-})
-
+import { LoginFormSchema, loginQuery } from "@/lib/api/login"
+import { useMutation } from "@tanstack/react-query"
+import { AxiosError } from "axios"
+import { useToast } from "@/components/ui/use-toast"
+import { GenericAxiosResponse } from "@/lib/plugins/axios/interface"
+import { Loader } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
 
 export default function LoginView() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const { toast } = useToast()
+    const navigate = useNavigate()
+    const form = useForm<z.infer<typeof LoginFormSchema>>({
+        resolver: zodResolver(LoginFormSchema),
         defaultValues: {
             password: "",
-            phone: 0
+            phone: ""
         }
     })
+    const queryKey = ['loginKey']
+    const { mutateAsync, isPending } = useMutation({
+        mutationKey: queryKey,
+        mutationFn: async (data: z.infer<typeof LoginFormSchema>) => await loginQuery(data),
+        onError: (error: AxiosError<GenericAxiosResponse>) => {
+            const message = error.response?.data?.message || 'Une erreur est survenue'
+            toast({
+                variant: 'destructive',
+                description: message,
+            })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-
-        console.log(values)
+        },
+        onSuccess: () => {
+            navigate('/commands')
+        }
+    })
+    async function onSubmit(values: z.infer<typeof LoginFormSchema>) {
+        await mutateAsync(values)
     }
     return (
         <div className="w-full min-h-svh min-h-screen flex flex-col justify-center items-center bg-black text-white">
@@ -35,11 +52,13 @@ export default function LoginView() {
                             <FormField
                                 control={form.control}
                                 name="phone"
+                                disabled={isPending}
                                 render={({ field }) => (
                                     <FormItem>
                                         <FormLabel className="font-light">Numéro de téléphone</FormLabel>
                                         <FormControl>
-                                            <Input type="tel" placeholder="699999999" {...field} />
+                                            <Input placeholder="699999999" {...field} />
+
                                         </FormControl>
                                     </FormItem>
                                 )}
@@ -47,6 +66,7 @@ export default function LoginView() {
                             <FormField
                                 control={form.control}
                                 name="password"
+                                disabled={isPending}
                                 render={({ field }) => (
                                     <FormItem>
                                         <FormLabel className="font-light">Mot de passe</FormLabel>
@@ -56,7 +76,10 @@ export default function LoginView() {
                                     </FormItem>
                                 )}
                             />
-                            <Button className="bg-white text-black rounded-full" type="submit">Se connecter</Button>
+                            <Button disabled={isPending} className="bg-white text-black rounded-full text-lg py-5" type="submit">
+                                Se connecter
+                                {isPending && <Loader size={18} className="animate-spin ml-3" />}
+                            </Button>
                         </form>
                     </Form>
                 </div>
