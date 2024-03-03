fd4ec746c9eb691da4351d338de37f02d8629578
ivan nzima <nzimaivan007@gmail.com>
2024-03-03 15:16:45 +0100

    update customer-list-item style when it is selected

--- src/components/app/customers/customer-list-item.tsx
+++ src/components/app/customers/customer-list-item.tsx
@@ -3,18 +3,25 @@ import { CustomersEntity } from "@/lib/types/entities";
 import React from "react";
 
 type CustomerListItemProps = {
-    customer: CustomersEntity
+    customer: CustomersEntity,
+    selected?: boolean,
 }
 
-export default function CustomerListItem({ customer, ...props }: CustomerListItemProps & React.HTMLProps<HTMLDivElement>) {
+export default function CustomerListItem({ customer, selected = false, ...props }: CustomerListItemProps & React.HTMLProps<HTMLDivElement>) {
 
     return (
         <Card
             onClick={props.onClick}
-            className={`cursor-pointer bg-inherit rounded-none shadow-none ${props.className}`}
+            className={`
+                bg-inherit rounded-none shadow-none ${props.className}
+                ${selected ? "" : "cursor-pointer transition-all duration-300 hover:text-black text-gray-600 hover:shadow-lg"}
+            `}
         >
             <CardHeader className="p-2 py-4">
-                <CardTitle className="font-normal text-gray-600 mb-2">
+                <CardTitle className={`
+                    font-normal  mb-2 
+                    ${selected ? "" : "transition-all duration-300 hover:font-medium hover:pl-2"}
+                `}>
                     {customer.name}
                 </CardTitle>
             </CardHeader>
